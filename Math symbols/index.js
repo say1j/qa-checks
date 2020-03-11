79df3e4c8da65b969a2d884b506bb8547dcd8077
say1j <31889581+say1j@users.noreply.github.com>
2020-03-11 20:43:26 +0200

    code refactoring

--- Math symbols/index.js
+++ Math symbols/index.js
@@ -4,7 +4,7 @@
     * @param {Array|Object} collection The collection of mathematical symbols.
     * @returns {Object} Returns the message with mismatch symbols and collection of fixes for extra mathematical symbols in translation.
     * @example
-    * 
+    *
     * Mathematical equation in source string: 1+2≠8
     * Mathematical equation in translation string: 1+2≠-8
     * // => 1+2≠8
@@ -31,108 +31,85 @@ if (crowdin.contentType == 'application/vnd.crowdin.text+plural') {
 }
 
 var translation = crowdin.translation
-
-if (characters.indexOf('-') === -1) {
-  regex = new RegExp('[' + characters.join('') + ']', 'g')
-} else {
-  regex = new RegExp('[\-' + RemoveCharacterFromArray('-').join('') + ']', 'g')
-}
+characters.indexOf('-') !== -1 ? characters[characters.indexOf('-')] = '\\-' : null
+regex = new RegExp('[' + characters.join('') + ']', 'g')
 
 sourceMatch = source.match(regex)
 translationMatch = translation.match(regex)
 
-function RemoveCharacterFromArray(characterForRemoving) {
-  var outputArray = []
-  for (var currentCharacter in characters) {
-    if (characters[currentCharacter] !== characterForRemoving) {
-      outputArray.push(characters[currentCharacter])
-    }
-  }
-  return outputArray
-}
-
 function UnionArrays (firstArray, secondArray) {
-  var tempArray = firstArray.concat(secondArray)
-  tempArray.sort()
-  var outputArray = [tempArray[0]]
-  for (var i = 1; i < tempArray.length; i++) {
-    if (tempArray[i] !== tempArray[i - 1]) {
-      outputArray.push(tempArray[i])
+  var outputArray = []
+  if (firstArray.length && secondArray.length) {
+    var tempArray = firstArray.concat(secondArray)
+    tempArray.sort()
+    outputArray = [tempArray[0]]
+    for (var i = 1; i < tempArray.length; i++) {
+      if (tempArray[i] !== tempArray[i - 1]) {
+        outputArray.push(tempArray[i])
+      }
     }
+    return outputArray
+  } else if (!firstArray.length) {
+    secondArray.length ? outputArray = secondArray : null
+  } else if (!secondArray.length) {
+    firstArray.length ? outputArray = firstArray : null
   }
   return outputArray
 }
 
-function ArrayContains (array, item) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === item) {
-      return true
-    }
-  }
-  return false
+function JoinWithQuotes (array, slice) {
+  return slice ? array.slice(0, 5).join('", "') + '" and others.' : '"' + array.join('", "') + '".'
 }
 
-if (sourceMatch != null || translationMatch != null) {
-  var sourceResult = {},
-      translationResult = {},
-      sourceProps = [],
-      translationProps = [],
-      mergedProps = [],
-      solution = []
-  if (sourceMatch != null && translationMatch != null) {
-    for (var i = 0; i < sourceMatch.length; ++i) {
-      var currentCharacter = sourceMatch[i]
-      if (sourceResult[currentCharacter] !== undefined) { ++sourceResult[currentCharacter] } else { sourceResult[currentCharacter] = 1 }
-    }
-    for (var j = 0; j < translationMatch.length; ++j) {
-      currentCharacter = translationMatch[j]
-      if (translationResult[currentCharacter] !== undefined) { ++translationResult[currentCharacter] } else { translationResult[currentCharacter] = 1 }
-    }
-    sourceProps = Object.getOwnPropertyNames(sourceResult)
-    translationProps = Object.getOwnPropertyNames(translationResult)
-    mergedProps = UnionArrays(sourceProps, translationProps)
+var sourceResult = {}
+var translationResult = {}
+var sourceProps = []
+var translationProps = []
+var mergedProps = []
+var solution = []
+
+if (sourceMatch != null) {
+  for (var i = 0; i < sourceMatch.length; ++i) {
+    var currentCharacter = sourceMatch[i]
+    sourceResult[currentCharacter] !== undefined ? ++sourceResult[currentCharacter] : sourceResult[currentCharacter] = 1
   }
-  if (sourceMatch != null || translationMatch != null) {
-    if (sourceMatch == null) {
-      for (j = 0; j < translationMatch.length; ++j) {
-        currentCharacter = translationMatch[j]
-        if (translationResult[currentCharacter] !== undefined) { ++translationResult[currentCharacter] } else { translationResult[currentCharacter] = 1 }
-      }
-      translationProps = Object.getOwnPropertyNames(translationResult)
-      mergedProps = translationProps
-    } else if (translationMatch == null) {
-      for (i = 0; i < sourceMatch.length; ++i) {
-        currentCharacter = sourceMatch[i]
-        if (sourceResult[currentCharacter] !== undefined) { ++sourceResult[currentCharacter] } else { sourceResult[currentCharacter] = 1 }
-      }
-      sourceProps = Object.getOwnPropertyNames(sourceResult)
-      mergedProps = sourceProps
-    }
+  sourceProps = Object.getOwnPropertyNames(sourceResult)
+}
+
+if (translationMatch != null) {
+  for (var j = 0; j < translationMatch.length; ++j) {
+    currentCharacter = translationMatch[j]
+    translationResult[currentCharacter] !== undefined ? ++translationResult[currentCharacter] : translationResult[currentCharacter] = 1
   }
-  var replacementArray = [],
-      missingArray = [],
-      extraArray = []
-  for (var u = 0; u < mergedProps.length; u++) {
-    if (ArrayContains(sourceProps, mergedProps[u]) && ArrayContains(translationProps, mergedProps[u])) {
-      if (sourceResult[mergedProps[u]] === translationResult[mergedProps[u]]) { // If match good, check next
-        continue
-      } else { // Sort extra/missing and add to array
+  translationProps = Object.getOwnPropertyNames(translationResult)
+}
+
+mergedProps = UnionArrays(sourceProps, translationProps)
+
+if (mergedProps.length) {
+  var replacementArray = []
+  var missingArray = []
+  var extraArray = []
+  for (var u = 0; u < mergedProps.length; u++) { // Sort extra/missing and add to array
+    if (sourceProps.indexOf(mergedProps[u]) !== -1 && translationProps.indexOf(mergedProps[u]) !== -1) {
+      if (sourceResult[mergedProps[u]] === translationResult[mergedProps[u]]) {
+      } else {
         if (sourceResult[mergedProps[u]] >= translationResult[mergedProps[u]]) {
           missingArray.push(mergedProps[u])
         } else if (sourceResult[mergedProps[u]] <= translationResult[mergedProps[u]]) {
           extraArray.push(mergedProps[u])
         }
       }
-    } else if (ArrayContains(sourceProps, mergedProps[u]) && !ArrayContains(translationProps, mergedProps[u])) { // Missed in translation, add to missing array
+    } else if (sourceProps.indexOf(mergedProps[u]) !== -1 && translationProps.indexOf(mergedProps[u]) === -1) {
       missingArray.push(mergedProps[u])
-    } else if (!ArrayContains(sourceProps, mergedProps[u]) && ArrayContains(translationProps, mergedProps[u])) { // Extra in translation, add to extra array
+    } else if ((sourceProps.indexOf(mergedProps[u]) === -1 && translationProps.indexOf(mergedProps[u]) !== -1)) {
       extraArray.push(mergedProps[u])
       replacementArray.push(mergedProps[u])
     }
   }
   if (replacementArray != null) { // Add fixes for extra symbols
-    var tempIndex,
-        repRegex = new RegExp('[' + replacementArray.join('') + ']', 'g')
+    var tempIndex
+    var repRegex = new RegExp('[' + replacementArray.join('') + ']', 'g')
     for (i = 0; i < replacementArray.length; i++) {
       for (j = 0; j < translationResult[replacementArray[i]]; j++) {
         tempIndex = repRegex.exec(translation).index
@@ -144,41 +121,21 @@ if (sourceMatch != null || translationMatch != null) {
       }
     }
   }
-  if (!(Array.isArray(extraArray) && extraArray.length) && !(Array.isArray(missingArray) && missingArray.length)) {
+  if (!extraArray.length && !missingArray.length) {
     result.success = true
-  } else if (Array.isArray(extraArray) && extraArray.length && Array.isArray(missingArray) && missingArray.length) {
+  } else {
     result.fixes = solution
-    result.message = 'Math symbols. Missing in translation: '
-    if (missingArray.length < 6) {
-      result.message += '"' + missingArray.join('", "') + '". '
-    } else {
-      result.message += '"' + missingArray.slice(0, 5).join('", "') + '" and others. '
+    result.message = 'Math symbols.'
+    if (missingArray.length) {
+      result.message += ' Missing in translation: '
+      missingArray.length < 6 ? result.message += JoinWithQuotes(missingArray, false) : result.message += JoinWithQuotes(missingArray, true)
     }
-    result.message += 'Extra in translation: '
-    if (extraArray.length < 6) {
-      result.message += '"' + extraArray.join('", "') + '".'
-    } else {
-      result.message += '"' + extraArray.slice(0, 5).join('", "') + '" and others.'
-    }
-  } else if (Array.isArray(missingArray) && missingArray.length && !(Array.isArray(extraArray) && extraArray.length)) {
-    result.fixes = solution
-    result.message = 'Math symbols. Missing in translation: '
-    if (missingArray.length < 6) {
-      result.message += '"' + missingArray.join('", "') + '".'
-    } else {
-      result.message += '"' + missingArray.slice(0, 5).join('", "') + '" and others.'
-    }
-  } else if (!(Array.isArray(missingArray) && missingArray.length) && Array.isArray(extraArray) && extraArray.length) {
-    result.fixes = solution
-    result.message = 'Math symbols. Extra in translation: '
-    if (extraArray.length < 6) {
-      result.message += '"' + extraArray.join('", "') + '".'
-    } else {
-      result.message += '"' + extraArray.slice(0, 5).join('", "') + '" and others.'
+    if (extraArray.length) {
+      result.message += ' Extra in translation: '
+      extraArray.length < 6 ? result.message += JoinWithQuotes(extraArray, false) : result.message += JoinWithQuotes(extraArray, true)
     }
   }
-} 
-else {
+} else {
   result.success = true
 }
 
