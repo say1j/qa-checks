98f1bfe89eacb50c6268ff1c30873709dd1090a6
say1j <31889581+say1j@users.noreply.github.com>
2020-03-12 19:56:16 +0200

    added check

---
+++ Spanish punctuation/test.js
@@ -0,0 +1,121 @@
+var crowdin = {
+    "sourceLanguage": "es",
+    "targetLanguage": "uk",
+    "context": {
+      "maxLength": 10
+    },
+    "contentType": "text/plain",
+    "source": "¿¿¡¡¡¿¿",
+    "translation": "!"
+}
+
+var config = {
+    "es": {
+        "¿": "?",
+        "¡": "!"
+    }
+}
+
+var result = {
+    success: false
+}
+
+if (crowdin.sourceLanguage !== "es") {
+    result.success = true
+    return result
+}
+
+if (crowdin.contentType === 'application/vnd.crowdin.text+plural') {
+    var obj = JSON.parse(crowdin.source)
+    if (obj[crowdin.context.pluralForm] != null) {
+      source = obj[crowdin.context.pluralForm]
+    } else {
+      source = obj.other
+    }
+  } else {
+    source = crowdin.source
+}
+
+var translation = crowdin.translation
+var sourceRegex = new RegExp('[' + Object.getOwnPropertyNames(config['es']).join('') + ']', 'g')
+var translationRegex = new RegExp('[' + Object.getOwnPropertyNames(config['es']).join('') + Object.values(config['es']).join('') + ']', 'g')
+
+var sourceMatch = source.match(sourceRegex)
+var translationMatch = translation.match(translationRegex)
+
+function JoinWithQuotes (array) {
+    return array.length < 5 ? '"' + array.join('", "') + '"' : array.slice(0, 5).join('", "') + '" and others'
+}
+
+function UniqueCharacters (inputArray) {
+    var outputArray = []
+    var currentCharacter
+    for (var i = 0; i < inputArray.length; i++) {
+      currentCharacter = inputArray[i]
+      if (!~outputArray.indexOf(currentCharacter)) {
+        outputArray.push(currentCharacter)
+      }
+    }
+    return outputArray
+}
+
+function IndicesCounter(inputCharracters) {
+    var indices = []
+    var outputArray = {}
+    var currentIndex
+    var uniqueInput = UniqueCharacters(inputCharracters)
+    for (var i = 0; i < uniqueInput.length; i++) {
+        var currentIndex = inputCharracters.indexOf(uniqueInput[i]);
+        while(currentIndex !== -1) {
+            indices.push(currentIndex);
+            currentIndex = inputCharracters.indexOf(uniqueInput[i], currentIndex + 1);
+        }
+        outputArray[uniqueInput[i]] = indices.length
+        indices = []
+    }
+    return outputArray
+}
+
+var sourceResult
+sourceMatch !== null ? sourceResult = IndicesCounter(sourceMatch) : null
+var translationResult = translationMatch !== null ? translationResult = IndicesCounter(translationMatch) : null
+var sourcePropertyNames = Object.getOwnPropertyNames(config['es'])
+var sourceResultPropertyNames
+sourceResult !== undefined ? sourceResultPropertyNames = Object.getOwnPropertyNames(sourceResult) : null
+var translationResultPropertyNames
+translationResult !== null ? translationResultPropertyNames = Object.getOwnPropertyNames(translationResult) : null
+var notAllowedInTranslationArray = []
+
+if (translationResult !== null) {
+    for (i = 0; i < sourcePropertyNames.length; i++) {
+        if (translationResultPropertyNames.indexOf(sourcePropertyNames[i]) !== -1) {
+            notAllowedInTranslationArray.push(sourcePropertyNames[i])
+        }
+    }
+} //Якщо є заборонені в перекладі повідомлення про те і не чекати решту
+if (notAllowedInTranslationArray.length) {
+    result.message = 'Spanish Punctuation. Please remove ' + JoinWithQuotes(notAllowedInTranslationArray) + ' from translation.'
+    result.fixes = [] // Дописати фікси
+} else if (sourceMatch !== null) {
+    var currentSymbol
+    var notLocalizedSymbols = []
+    if (translationResult === null) { //// ++++++++++++++
+        for (i = 0; i < sourceResultPropertyNames.length; i++) {
+            notLocalizedSymbols.push(sourceResultPropertyNames[i])
+        }
+        result.message = 'Spanish Punctuation. Please localize next punctuation symbol(s): ' + JoinWithQuotes(notLocalizedSymbols) + '.'
+    } else { // Вивести розбіжності
+        for (i = 0; i < sourcePropertyNames.length; i++) {
+            if (sourceResult[sourcePropertyNames[i]] > translationResult[config["es"][sourcePropertyNames[i]]]) {
+                notLocalizedSymbols.push('"' + sourcePropertyNames[i] + '" doesn`t localized ' + (sourceResult[sourcePropertyNames[i]] - translationResult[config["es"][sourcePropertyNames[i]]]) + ' time(s)')
+            } else if (translationResult[config["es"][sourcePropertyNames[i]]] === undefined) {
+                notLocalizedSymbols.push('"' + sourcePropertyNames[i] + '" doesn`t localized ' + sourceResult[sourcePropertyNames[i]] + ' time(s)')
+            }
+        }
+        result.message = 'Spanish Punctuation. Next issue(s) found: ' + notLocalizedSymbols.join(', ') + '.'
+    }
+} else {
+    result.success = true
+}
+console.debug(result)
+return result
