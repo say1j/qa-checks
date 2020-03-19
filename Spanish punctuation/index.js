0c4a62d5123ad6f9ac4aed915410e1f42ba2afc3
say1j <31889581+say1j@users.noreply.github.com>
2020-03-19 14:19:20 +0200

    Update index.js

--- Spanish punctuation/index.js
+++ Spanish punctuation/index.js
@@ -1,15 +1,13 @@
 var config = {
-  "es": {
-    "¿": "?",
-    "¡": "!"
-  }
+  "¿": "?",
+  "¡": "!"
 }
 
 var result = {
   success: false
 }
 
-if (crowdin.targetLanguage !== "es") {
+if (crowdin.sourceLanguage !== "es") {
   result.success = true
   return result
 }
@@ -26,8 +24,8 @@ if (crowdin.contentType === 'application/vnd.crowdin.text+plural') {
 }
 
 var translation = crowdin.translation
-var sourceRegex = new RegExp('[' + Object.values(config['es']).join('') + ']', 'g')
-var translationRegex = new RegExp('[' + Object.getOwnPropertyNames(config["es"]).join('') + ']', 'g')
+var sourceRegex = new RegExp('[' + Object.getOwnPropertyNames(config).join('') + ']', 'g')
+var translationRegex = new RegExp('[' + Object.getOwnPropertyNames(config).join('') + Object.values(config).join('') + ']', 'g')
 
 var sourceMatch = source.match(sourceRegex)
 var translationMatch = translation.match(translationRegex)
@@ -68,41 +66,44 @@ function IndicesCounter(inputCharracters) {
 var sourceResult
 sourceMatch !== null ? sourceResult = IndicesCounter(sourceMatch) : null
 var translationResult = translationMatch !== null ? translationResult = IndicesCounter(translationMatch) : null
-var sourcePropertyValues = Object.values(config["es"])
-var sourcePropertyNames = Object.getOwnPropertyNames(config["es"])
+var sourcePropertyNames = Object.getOwnPropertyNames(config)
 var sourceResultPropertyNames
 sourceResult !== undefined ? sourceResultPropertyNames = Object.getOwnPropertyNames(sourceResult) : null
 var translationResultPropertyNames
 translationResult !== null ? translationResultPropertyNames = Object.getOwnPropertyNames(translationResult) : null
-var notAllowedInSourceArray = []
-if (sourceMatch !== null) {
+var notAllowedInTranslationArray = []
+
+if (translationResult !== null) {
+  for (i = 0; i < sourcePropertyNames.length; i++) {
+    if (translationResultPropertyNames.indexOf(sourcePropertyNames[i]) !== -1) {
+      notAllowedInTranslationArray.push(sourcePropertyNames[i])
+    }
+  }
+}
+if (notAllowedInTranslationArray.length) {
+  result.message = 'Spanish Punctuation. Please remove ' + JoinWithQuotes(notAllowedInTranslationArray) + ' from translation.'
+  result.fixes = [] // will be fixes for removing
+} else if (sourceMatch !== null) {
   var currentSymbol
   var notLocalizedSymbols = []
-  if (translationResult === null) {
+  if (translationResult === null) { //// ++++++++++++++
+    for (i = 0; i < sourceResultPropertyNames.length; i++) {
+      notLocalizedSymbols.push(sourceResultPropertyNames[i])
+    }
     result.fixes = []
-    result.message = 'Spanish Punctuation. Please localize next punctuation symbol(s): ' + JoinWithQuotes(sourceResultPropertyNames) + '.'
+    result.message = 'Spanish Punctuation. Please localize next punctuation symbol(s): ' + JoinWithQuotes(notLocalizedSymbols) + '.'
   } else { // Output mismatch
-    for (i = 0; i < sourcePropertyValues.length; i++) {
-      if (translationResult[sourcePropertyNames[sourcePropertyValues.indexOf(sourcePropertyValues[i])]] < sourceResult[sourcePropertyValues[i]]) {
-        notLocalizedSymbols.push('"' + [sourcePropertyValues[i]] + '" doesn`t localized ' + (sourceResult[sourcePropertyValues[i]] - translationResult[sourcePropertyNames[sourcePropertyValues.indexOf(sourcePropertyValues[i])]]) + ' time(s)')
-      } else if (translationResult[sourcePropertyNames[sourcePropertyValues.indexOf(sourcePropertyValues[i])]] > sourceResult[sourcePropertyValues[i]]) {
-        notLocalizedSymbols.push('"' + [sourcePropertyValues[i]] + '" extra localized ' + (translationResult[sourcePropertyNames[sourcePropertyValues.indexOf(sourcePropertyValues[i])]] - sourceResult[sourcePropertyValues[i]]) + ' time(s)')
-      } else if (translationResult[sourcePropertyNames[sourcePropertyValues.indexOf(sourcePropertyValues[i])]] === undefined) {
-        notLocalizedSymbols.push('"' + sourceResult[config["es"][[sourcePropertyValues[i]]]] + '" doesn`t localized ' + sourceResult[sourcePropertyValues[i]] + ' time(s)')
-      } else if (sourceResult[sourcePropertyValues[i]] === undefined) {
-        notLocalizedSymbols.push('"' + [sourcePropertyValues[i]] + '" extra localized ' + translationResult[sourcePropertyNames[sourcePropertyValues.indexOf(sourcePropertyValues[i])]] + ' time(s)')
+    for (i = 0; i < sourcePropertyNames.length; i++) {
+      if (sourceResult[sourcePropertyNames[i]] > translationResult[config[sourcePropertyNames[i]]]) {
+        notLocalizedSymbols.push('"' + sourcePropertyNames[i] + '" doesn`t localized ' + (sourceResult[sourcePropertyNames[i]] - translationResult[config[sourcePropertyNames[i]]]) + ' time(s)')
+      } else if (translationResult[config[sourcePropertyNames[i]]] === undefined) {
+        notLocalizedSymbols.push('"' + sourcePropertyNames[i] + '" doesn`t localized ' + sourceResult[sourcePropertyNames[i]] + ' time(s)')
       }
     }
-    if (!notLocalizedSymbols.length) {
-      result.success = true
-      return result
-    } else {
-      result.fixes = []
-      result.message = 'Spanish Punctuation. Next issue(s) found: ' + notLocalizedSymbols.join(', ') + '.'
-    }
+    result.fixes = []
+    result.message = 'Spanish Punctuation. Next issue(s) found: ' + notLocalizedSymbols.join(', ') + '.'
   }
 } else {
   result.success = true
 }
-
 return result
